22f071bd5856ce4b121d6c2b783a4099daeb3326
Murphy'sOutlaws <firstteam677@columbusschoolforgirls.org>
2026-02-16 11:51:33 -0500

    Create shooter configs

--- src/main/java/frc/robot/Configs.java
+++ src/main/java/frc/robot/Configs.java
@@ -7,46 +7,80 @@ import frc.robot.Constants.SwerveConstants;
 
 public class Configs {
 
-        public static final class MAXSwerveModule {
-                public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
-                public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
+    public static final class MAXSwerveModule {
+        public static final SparkMaxConfig drivingConfig = new SparkMaxConfig();
+        public static final SparkMaxConfig turningConfig = new SparkMaxConfig();
 
-                static {
-                        // Use module constants to calculate conversion factors and feed forward gain.
-                        drivingConfig
-                                        .idleMode(IdleMode.kBrake)
-                                        .smartCurrentLimit(50);
-                        drivingConfig.encoder
-                                        .positionConversionFactor(SwerveConstants.ENCODER_TO_METERS_DRIVE_RATIO) // meters
-                                        .velocityConversionFactor(SwerveConstants.ENCODER_TO_METERS_DRIVE_RATIO / 60.0); // meters
-                                                                                                                         // per
-                                                                                                                         // second
-                        drivingConfig.closedLoop
-                                        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                        // These are example gains you may need to them for your own robot!
-                                        .pid(0.04, 0, 0)
-                                        .outputRange(-1, 1);
+        static {
+            // Use module constants to calculate conversion factors and feed forward gain.
+            drivingConfig
+                    .idleMode(IdleMode.kBrake)
+                    .smartCurrentLimit(50);
+            drivingConfig.encoder
+                    .positionConversionFactor(SwerveConstants.ENCODER_TO_METERS_DRIVE_RATIO) // meters
+                    .velocityConversionFactor(SwerveConstants.ENCODER_TO_METERS_DRIVE_RATIO / 60.0); // meters/second
+            drivingConfig.closedLoop
+                    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                    // TODO: Adjust PID values for our robot
+                    .pid(0.04, 0, 0)
+                    .outputRange(-1, 1);
 
-                        turningConfig
-                                        .idleMode(IdleMode.kBrake)
-                                        .smartCurrentLimit(20);
-                        turningConfig.encoder
-                                        .positionConversionFactor(SwerveConstants.ENCODER_TO_METERS_TURN_RATIO) // radians
-                                        .velocityConversionFactor(SwerveConstants.ENCODER_TO_METERS_TURN_RATIO / 60.0); // radians
-                                                                                                                        // per
-                                                                                                                        // second
-                        turningConfig.closedLoop
-                                        .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
-                                        .pid(0.5, 0, 0.0)
-                                        .outputRange(-1, 1)
-                                        // Enable PID wrap around for the turning motor. This will allow the PID
-                                        // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
-                                        // to 10 degrees will go through 0 rather than the other direction which is a
-                                        // longer route.
-                                        .positionWrappingEnabled(true)
-                                        .positionWrappingInputRange(0, 2 * Math.PI);
+            turningConfig
+                    .idleMode(IdleMode.kBrake)
+                    .smartCurrentLimit(20);
+            turningConfig.encoder
+                    .positionConversionFactor(SwerveConstants.ENCODER_TO_METERS_TURN_RATIO) // radians
+                    .velocityConversionFactor(SwerveConstants.ENCODER_TO_METERS_TURN_RATIO / 60.0); // radians/second
+            turningConfig.closedLoop
+                    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                    .pid(0.5, 0, 0.0)
+                    .outputRange(-1, 1)
+                    // Enable PID wrap around for the turning motor. This will allow the PID
+                    // controller to go through 0 to get to the setpoint i.e. going from 350 degrees
+                    // to 10 degrees will go through 0 rather than the other direction which is a
+                    // longer route.
+                    .positionWrappingEnabled(true)
+                    .positionWrappingInputRange(0, 2 * Math.PI);
 
-                        turningConfig.closedLoop.maxMotion.allowedProfileError(0.04);
-                }
+            turningConfig.closedLoop.maxMotion.allowedProfileError(0.04);
         }
+    }
+
+    public static final class ShooterSystem {
+        public static final SparkMaxConfig shooterConfig = new SparkMaxConfig();
+        public static final SparkMaxConfig shooterFollowerConfig = new SparkMaxConfig();
+        public static final SparkMaxConfig feederConfig = new SparkMaxConfig();
+        public static final SparkMaxConfig rollersConfig = new SparkMaxConfig();
+
+        static {
+            shooterConfig
+                    .inverted(true)
+                    .idleMode(IdleMode.kCoast)
+                    .smartCurrentLimit(40);
+            shooterConfig.closedLoop
+                    .feedbackSensor(FeedbackSensor.kPrimaryEncoder)
+                    // TODO: Adjust PID values for our robot
+                    .pid(0.04, 0, 0)
+                    .outputRange(-1, 1);
+            shooterConfig.closedLoop.maxMotion
+                    .cruiseVelocity(5000)
+                    .maxAcceleration(10000)
+                    .allowedProfileError(1);
+
+            shooterFollowerConfig.apply(shooterConfig)
+                    .follow(Constants.ShooterConstants.SHOOTER_MOTOR_ID, true);
+
+            feederConfig
+                    .inverted(true) // TODO: Update depending on direction of feeder motors
+                    .idleMode(IdleMode.kBrake)
+                    .openLoopRampRate(1.0)
+                    .smartCurrentLimit(60);
+
+            rollersConfig
+                    .inverted(true) // TODO: Update depending on direction of roller motors
+                    .idleMode(IdleMode.kBrake)
+                    .openLoopRampRate(0.5)
+                    .smartCurrentLimit(40);
+        }
+    }
 }
