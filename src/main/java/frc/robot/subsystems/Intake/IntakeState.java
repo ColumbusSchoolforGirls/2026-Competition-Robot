b07c2a9d3f8d5906c6065da4e307248a4f117628
keix13 <kleff@columbusschoolforgirls.org>
2026-02-28 12:01:30 -0500

    Intake state fix

--- src/main/java/frc/robot/subsystems/Intake/IntakeState.java
+++ src/main/java/frc/robot/subsystems/Intake/IntakeState.java
@@ -1,10 +1,5 @@
 package frc.robot.subsystems.Intake;
 
 public enum IntakeState {
-    IN {
-        public IntakeState nextState() {
-            return DEPLOYING;
-        }
-    }
-    , DEPLOYING, OUT, RETRACTING
+    IN, DEPLOYING, OUT, RETRACTING
 }
