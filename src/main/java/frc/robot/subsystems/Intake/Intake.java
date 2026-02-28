3c2ed11d5ac110ae8a53e50f5e1601977a04a529
Murphy'sOutlaws <firstteam677@columbusschoolforgirls.org>
2026-02-28 10:55:36 -0500

    Intake encoder math

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -3,15 +3,15 @@ package frc.robot.subsystems.Intake;
 import frc.robot.Configs;
 import frc.robot.Constants.IntakeConstants;
 
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
-import com.revrobotics.spark.SparkMax;
 import com.revrobotics.PersistMode;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
-
-import edu.wpi.first.wpilibj.DigitalInput;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import com.revrobotics.spark.SparkMax;
 
 public class Intake {
     private final SparkMax deployMotor = new SparkMax(IntakeConstants.DEPLOY_ID, MotorType.kBrushless);
@@ -23,6 +23,7 @@ public class Intake {
 
     private IntakeState state;
     private boolean runRoller;
+    private double positionDifference;
 
     public Intake() {
         deployMotor.configure(
@@ -38,24 +39,17 @@ public class Intake {
         updateDashboard();
     }
 
-    // TODO: Implement the isDeployed() and isRetracted() using encoder ticks.
     public boolean isRetracted() {
-        return retractedLimitSwitch.get();
+        if (retractedLimitSwitch.get()) {
+            deployEncoder.setPosition(0);
+            return true;
+        }
+        return false;
     }
 
     public boolean isDeployed() {
-        return deployEncoder.getPosition() >= IntakeConstants.DEPLOYED_TICK_DISTANCE;
-    }
-
-    // TODO: Move the arm
-    private void deployIntake() {
-        
-    }
-
-    private void resetIntakeEncoder() {
-        if (isRetracted()) {
-            deployEncoder.setPosition(0);
-        }
+        positionDifference = IntakeConstants.DEPLOYED_TICK_DISTANCE - deployEncoder.getPosition();
+        return positionDifference <= 0;
     }
 
     private void updateDashboard() {
@@ -70,18 +64,23 @@ public class Intake {
 
     private double determineDeployPercentageOutput() {
         if (state == IntakeState.DEPLOYING) {
-            return IntakeConstants.DEPLOY_PERCENTAGE_OUTPUT;
+            if (!isDeployed()) {
+                return IntakeConstants.DEPLOY_PERCENTAGE_OUTPUT;
+            }
+            return 0;
         } else if (state == IntakeState.RETRACTING) {
-            return -IntakeConstants.DEPLOY_PERCENTAGE_OUTPUT;
+            if (!isRetracted()) {
+                return -IntakeConstants.DEPLOY_PERCENTAGE_OUTPUT;
+            }
+            return 0;
         }
         return 0;
     }
 
     private double determineRollerPercentageOutput() {
-        if (state == IntakeState.OUT && runRoller == true) {
+        if (runRoller) {
             return IntakeConstants.ROLLER_PERCENTAGE_OUTPUT;
-        } else {
-            return 0.0;
         }
+        return 0;
     }
 }
